0133ff75f45af4943fd9328f8cf10c7fecada0cb
exelCode226 <exelagualimpia@gmail.com>
2025-01-30 16:54:45 -0600

    Se realizaron las operaciones basicas de CRUD

--- src/controllers/productsCotrollers.js
+++ src/controllers/productsCotrollers.js
@@ -39,13 +39,96 @@ export const postProducts = async (req, res) => {
 }
 
 export const getOneProduct = async (req, res) => {
-  res.send("GetOneProduct");
+    const id=parseInt(req.params.id);
+  try {
+    if (!id) {
+        return res.status(400).send("Ingresa un id Valido")
+    }
+    const existing=await prisma.products.findFirst({
+        where:{
+            id:id
+        }
+    
+       })
+       if (!existing) {
+        return res.status(400).send("Este recurso no se encuentra en la base de datos")
+    
+       }
+    await prisma.products.delete({
+        where:{
+            id:id
+        }
+    })
+    res.status(200).send("Eliminado con exito...")
+  } catch (error) {
+    console.error(error)
+    return res.status(500).send("Error en el servidor")  
+  }
 };
 
 export const putProducts = async (req, res) => {
-  
+  const id=parseInt(req.params.id)
+  const {name,description,idCategory}=req.body
+  try {
+    if (!id) {
+        return res.status(400).send("Ingresa un id Valido")
+    }
+    if (!name || !description || !idCategory) {
+        return res.status(400).send("Ingresa todos los campos")
+
+    }
+   const existing=await prisma.products.findFirst({
+    where:{
+        id:id
+    }
+
+   })
+   if (!existing) {
+    return res.status(400).send("Este recurso no se encuentra en la base de datos")
+
+   }
+
+    const product= await prisma.products.update({
+        where:{
+            id:id
+        },
+        data:{
+            name,
+            description,
+            idCategory
+        }
+    })
+    return res.status(200).json({success:true,data:product})
+  } catch (error) {
+    console.error(error)
+    return res.status(500).send("Error en el servidor")
+  }
 };
 
 export const deleteProducts = async (req, res) => {
-  res.send("deleteProducts");
+  const id=parseInt(req.params.id);
+  try {
+    if (!id) {
+        return res.status(400).send("Ingresa un id Valido")
+    }
+    const existing=await prisma.products.findFirst({
+        where:{
+            id:id
+        }
+    
+       })
+       if (!existing) {
+        return res.status(400).send("Este recurso no se encuentra en la base de datos")
+    
+       }
+    await prisma.products.delete({
+        where:{
+            id:id
+        }
+    })
+    res.status(200).send("Eliminado con exito...")
+  } catch (error) {
+    console.error(error)
+    return res.status(500).send("Error en el servidor")  
+  }
 };
